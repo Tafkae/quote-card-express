2665e6b7f9449cf4816451244720912408abd000
Lisa H. <neuroticNanobot@outlook.com>
2025-02-09 19:00:05 -0500

    tried to implement non-hard-coded API Key in server.js but it doesn't work :T

--- public/js/index.js
+++ public/js/index.js
@@ -1,2 +1,33 @@
-//JavaScript
 "use strict";
+
+const el = {
+  quote: document.getElementById("quote-content"),
+  author: document.getElementById("quote-author"),
+  randomBtn: document.getElementById("random-btn")
+};
+
+async function getRandomPhoto() {
+
+  try {
+    let receivedPhoto = await fetch("/api/random-bg");
+    // let data = await receivedPhoto.data;
+    console.log(receivedPhoto);
+    // el.quote.setAttribute("style",`background-image:url(${data.url.regular}) !important;`)
+
+    // const imgDiv = document.querySelector("main.main.background-img");
+    // console.log(imgDiv);
+
+    //imgDiv.computedStyleMap.backgroundImage = `url(${receivedPhoto})`;
+
+
+
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+el.randomBtn.addEventListener("click", async () => {
+  await getRandomPhoto();
+})
+
+
--- server.js
+++ server.js
@@ -1,18 +1,60 @@
 "use strict";
 
+/* API relay code from https://www.freecodecamp.org/news/private-api-keys/
+ */
 const express = require("express");
-const app = express();
+require("dotenv").config();
 const path = require("path");
+const rateLimit = require("express-rate-limit");
+const app = express();
 const port = 1776;
 
 // Serves front-end content in the public directory
 app.use("", express.static(path.join(__dirname, "./public")));
 
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
+
+// Rate limit (make sure I don't send a jillion requests to Unsplash)
+// Demo mode limits to 50 json requests per hour.
+const limiter = rateLimit({
+  windowMs: 1000 * 3600,
+  max: 50,
+});
+app.use(limiter); // apply rate limiter to all requests
+
+// test route, use to verify it works
+// localhost:PORT/hello
+app.get("/hello", (req, res) =>
+  res.send(
+    "<html><head><title>Hello World</title></head><body><h1>Hello world</h1></body></html>"
+  )
+);
+
+// Unsplash API relay route
+app.get("/api/random-bg", async (req, res) => {
+  try {
+    const response = await fetch(
+      `https://api.unsplash.com/photos/random/?client_id=${process.env.UNSPLASH_API_KEY}`
+    );
+    const data = await response.json();
+    if (data.errors) console.log(data.errors);
+    const receivedPhoto = data; //.urls.regular;
+
+    return res.json({
+      success: true,
+      data: receivedPhoto,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
 
 // Serves the whole app
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
-  console.log(`Press Ctrl+C to end this process.`)
+  console.log(`Press Ctrl+C to end this process.`);
 });
